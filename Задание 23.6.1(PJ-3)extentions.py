07abf5be5639f82021042b382a28ce4bbe25ff9a
ТатьянаЩелокова <126164810+TatianaShchelok@users.noreply.github.com>
2023-10-27 19:39:57 +0500

    Create Задание 23.6.1(PJ-3)extentions.py
    File extentions для Алёна Телебот

---
+++ Задание 23.6.1(PJ-3)extentions.py
@@ -0,0 +1,29 @@
+from config import keys
+
+
+class ConvertionExeption(Exception):
+    pass
+
+
+class FiatConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionExeption('Обрабатывать одинаковые валюты нет смысла')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionExeption(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionExeption(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionExeption(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base * amount
